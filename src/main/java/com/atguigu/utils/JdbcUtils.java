6beecf9a9e8b34f527687e190d236ee4904b4bab
YY1661512225 <1661512225@qq.com>
2022-09-30 14:04:41 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/utils/JdbcUtils.java
@@ -0,0 +1,19 @@
+package com.atguigu.utils;
+
+import java.sql.Connection;
+
+public class JdbcUtils {
+    /*
+    获取数据库连接池中的连接
+     */
+    public static Connection getConnection(){
+        return null;
+    }
+
+    /*
+    关闭连接，放回数据连接池
+     */
+    public static void close(Connection conn){
+
+    }
+}
